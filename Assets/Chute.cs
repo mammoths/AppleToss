07133e0da4de41299a2ffa25c64a2550d4fbbb90
mammoths <emailmichellevo@gmail.com>
2017-06-28 10:02:03 -0700

    First commit

---
+++ Assets/Chute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chute : MonoBehaviour {
+    public GameObject prefab;
+    public Transform spawnpoint;
+    public bool needNewSpawn;
+	// Use this for initialization
+	void Start () {
+        needNewSpawn = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (needNewSpawn)
+        { Spawn(); }
+        
+      
+        
+    
+    }
+
+
+    void Spawn()
+    {
+        GameObject spawn = (GameObject)GameObject.Instantiate(prefab, spawnpoint.transform.position, Quaternion.identity);
+        needNewSpawn = false; 
+        
+        if (spawn.transform.position != spawnpoint.transform.position)
+        {
+            needNewSpawn = true; 
+        }
+    }
+}
+
+
+
